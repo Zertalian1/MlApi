4a2cdf2ce4a934be240edd34e8aad253bf34db3a
Zertalian1 <d.kurdyukov@g.nsu.ru>
2023-12-30 20:37:14 +0700

    Добавил контейнеризацию

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # установка базового образа (host OS)
-FROM python:3.11
+FROM python:3.9
 
 # установка рабочей директории в контейнере
 WORKDIR /code
@@ -11,7 +11,4 @@ COPY requirements.txt .
 RUN pip install -r requirements.txt
 
 # копирование содержимого локальной директории src в рабочую директорию
-COPY src/ .
-
-# команда, выполняемая при запуске контейнера
-CMD [ "python", "./server.py" ]
+COPY . .
--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from domain.models import db, Product, ProductHistory
 from models.ProductCountPredictModelService import ProductCountPredictModelService, ProductCountPredictInput
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://Zertallian:password@localhost:5433/MlDb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://Zertallian:password@mlPostgres:5432/MlDb'
 app.config['SECRET_KEY'] = 'secret-key'
 db.init_app(app)
 model = ProductCountPredictModelService()
@@ -125,7 +125,7 @@ def predict_food_balance():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 with app.app_context():
     db.create_all()
